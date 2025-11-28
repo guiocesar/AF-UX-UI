8ed4244d4329ccafe3cc209b808862cdfd234799
Guilherme Cesar <127548558+guiocesar@users.noreply.github.com>
2025-11-28 20:43:44 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,26 @@
+package calculadora;
+
+public class Calculadora{
+
+    public int r = 0;
+
+    public int calc(int a, int b, String op){
+
+        //Calculadora
+
+        if(op.equals(anObject: "+")){
+
+            r = a + b;
+        }else if(op.equals(anObject: "-")){
+            r = a - b;
+        }else if(op.equals(anObject: "*")){
+            r = a * b;
+
+        }
+    }
+
+
+
+
+
+}
