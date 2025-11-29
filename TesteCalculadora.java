1767980cbc9fc7a7902b90b312bd1308e183afc6
Guilherme Cesar <127548558+guiocesar@users.noreply.github.com>
2025-11-28 21:27:49 -0300

    Refactor TesteCalculadora to use calcular method

--- TesteCalculadora.java
+++ TesteCalculadora.java
@@ -5,9 +5,23 @@ public class TesteCalculadora{
 public static void main(String[] args){
     Calculadora calc = new Calculadora();
 
-    //Testes rapidos
-    calc.calc( 2, 3 ,"+");
-    calc.calc(10, 4, "-");
-    calc.
-}
+    System.out.println(calc.calcular(2, 3, "+"));
+    System.out.println(calc.calcular(10, 4, "-"));
+    System.out.println(calc.calcular(3, 5, "*"));
+    System.out.println(calc.calcular(8, 2, "/"));
+
+    try{
+        System.out.println(calc.calcular(8, 0, "/"));// exceção
+    } catch (IllegalArgumentException e){
+        System.out.println(e.getMessage());
+    }
+
+    try{
+        System.out.println(calc.calcular(5, 5, "x"));// exceção
+    } catch (IllegalArgumentException e){
+        System.out.println(e.getMessage());
+    }
+
+
+    }
 }
