29ac9b331567022009d739633e1de229a44dd0c5
Guilherme Cesar <127548558+guiocesar@users.noreply.github.com>
2025-11-28 21:28:34 -0300

    Refactor Calculadora class with operation methods
    Refactored the Calculadora class to include separate methods for each operation and improved error handling for division.

--- Calculadora.java
+++ Calculadora.java
@@ -2,25 +2,33 @@ package calculadora;
 
 public class Calculadora{
 
-    public int r = 0;
-
-    public int calc(int a, int b, String op){
-
-        //Calculadora
-
-        if(op.equals(anObject: "+")){
-
-            r = a + b;
-        }else if(op.equals(anObject: "-")){
-            r = a - b;
-        }else if(op.equals(anObject: "*")){
-            r = a * b;
-
-        }
+    public int somar(int a, int b){
+        return a + b;
     }
 
+    public int subtrair(int a, int b){
+        return a - b;
+    }
 
+    public int multiplicar(int a, int b){
+        return a * b;
+    }
 
+    public int dividir(int a, int b){
+        if (b == 0) {
+            throw new IllegalArgumentException("Divisão por zero não é permitida");
+        }
+        return a / b; 
+    }
 
-
+    public int calcular(int a, int b, String operador){
+        return switch (operador){
+            case "+" -> somar(a, b);
+            case "-" -> subtrair(a, b);
+            case "*" -> multiplicar(a, b);
+            case "/" -> dividir(a, b);
+            default -> throw new IllegalArgumentException("Operador Invalido" + operador);
+        };
+    }
+    
 }
